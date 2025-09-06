22f0aaef0dd7ba9aa7643aa98b4f26474e93e5e0
DarshanCHMSR <chdarshan99@gmail.com>
2025-09-06 09:34:07 +0530

    changed the frontend from the react to react vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
@@ -8,14 +7,14 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
